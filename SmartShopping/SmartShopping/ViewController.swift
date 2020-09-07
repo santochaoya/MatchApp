650dc963aa4a47fdfed06f98e8687e3860a46eed
santochaoya <santochaoya@yahoo.com.hk>
2020-09-07 13:44:47 +1200

    newProject

---
+++ SmartShopping/SmartShopping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartShopping
+//
+//  Created by santochaoya on 7/09/20.
+//  Copyright © 2020 Xiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
